dcf5ab73e86b060bd2f491eb96555b71c995f8f5
subrat.sahu <subrat.sahu@BRILAPNB5575.BRILLIO.COM>
2024-02-14 13:48:46 +0530

    Power of 2

--- src/main/java/com/java/intvw/PowerOf2.java
+++ src/main/java/com/java/intvw/PowerOf2.java
@@ -1,10 +1,38 @@
 package com.java.intvw;
 
+import java.util.Scanner;
+
+//Check power of 2
 public class PowerOf2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter no :");
+		Integer num  = sc.nextInt() ;
+		
+		//using old java
+		power2(num);
+	}
 
+	private static void power2(Integer num) {
+		Boolean flag = false;
+		while(true) {
+			if(num%2 == 0) {
+				num = num /2;
+				if(num == 1) {
+					flag = true;
+					break;
+				}
+			} else {
+				break;
+			}
+		}
+		
+		if(flag) 
+			System.out.println("power of 2");
+		else {
+			System.out.println("not");
+		}
 	}
 
 }
--- src/main/java/com/java/test/Testx.java
+++ src/main/java/com/java/test/Testx.java
@@ -4,8 +4,8 @@ public class Testx {
 
 	
 	public static void main(String[] args) {
-		System.out.println("-----------");
-
+ 		System.out.println("-----------");
+		
 	}
 
 }
