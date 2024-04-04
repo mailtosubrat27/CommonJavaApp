4c68d4273a5d1482828a35e5341ae66f31cf764d
subrat.sahu <mailtosubrat27@gmail.com>
2024-04-04 10:47:46 +0530

    merge list

---
+++ src/main/java/com/java/intvw/AddTwoListAndFindDuplicate.java
@@ -0,0 +1,21 @@
+package com.java.intvw;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AddTwoListAndFindDuplicate {
+
+	public static void main(String[] args) {
+		List<Integer> l1 = Arrays.asList(11,13,14,18,15);
+		List<Integer> l2 = Arrays.asList(11,22,12,13,15);
+		
+		findDuplicateFromList(l1, l2);
+
+	}
+
+	private static void findDuplicateFromList(List<Integer> l1, List<Integer> l2) {
+//		l1.stream().flatMap(a -> )
+		
+	}
+
+}
