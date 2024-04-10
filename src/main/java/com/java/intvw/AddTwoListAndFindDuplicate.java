cdcdcf38bfc83979dff9a89d8a74b3bdecbe37c7
subrat.sahu <mailtosubrat27@gmail.com>
2024-04-10 15:13:56 +0530

    Add list

--- src/main/java/com/java/intvw/AddTwoListAndFindDuplicate.java
+++ src/main/java/com/java/intvw/AddTwoListAndFindDuplicate.java
@@ -1,7 +1,13 @@
 package com.java.intvw;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AddTwoListAndFindDuplicate {
 
@@ -10,11 +16,39 @@ public class AddTwoListAndFindDuplicate {
 		List<Integer> l2 = Arrays.asList(11,22,12,13,15);
 		
 		findDuplicateFromList(l1, l2);
+		
+		//add two list
+		addTwoList(l1, l2);
 
 	}
 
+	private static void addTwoList(List<Integer> l1, List<Integer> l2) {
+		List<Integer> newList = Stream.of(l1, l2)
+				.flatMap(Collection :: parallelStream)
+				.collect(Collectors.toList());
+		
+		System.out.println("1- Add two Lists using flatmap : "+ newList);
+				
+		List<Integer> newList1 = Stream.concat(l1.stream(), l2.stream())
+				.toList();
+		System.out.println("2- Add two Lists using concat : "+ newList1);
+		
+		Stream.generate(new Random() :: nextInt)
+		.limit(5)
+		.forEach(System.out :: println);
+	}
+
 	private static void findDuplicateFromList(List<Integer> l1, List<Integer> l2) {
-//		l1.stream().flatMap(a -> )
+		List<Integer> newList = Stream.of(l1, l2)
+				.flatMap(Collection :: parallelStream)
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				.entrySet()
+				.stream()
+				.filter(p -> p.getValue()>1)
+				.map( Map.Entry::getKey )
+				   .collect( Collectors.toList() );
+		
+		System.out.println("1- Duplicate List : "+ newList);
 		
 	}
 
